ad8672ff1b9de91c2ad62302f7aa79fc98a542e7
Patrick Madlang-awa <madlangawapat22@gmail.com>
2026-02-17 22:34:32 +0800

    cloudinary fixed

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    // domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'github.com' },
+    ],
+
+    //   loader: 'custom',
+    //   loaderFile: './app/image.ts',
+  },
   // Your Next.js config here
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
