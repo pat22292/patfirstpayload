8673cbfb3e06bc00db1d9505b39ba81639cbbe78
Patrick Madlang-awa <madlangawapat22@gmail.com>
2026-02-22 09:42:05 +0800

    test

--- src/app/(frontend)/product/page.tsx
+++ src/app/(frontend)/product/page.tsx
@@ -9,34 +9,44 @@ type PageProps = {
   }>
 }
 
-export default async function Product({ searchParams }: PageProps) {
+export default async function Product({ searchParams,  }: PageProps) {
    const { id } = await searchParams
 
   if (!id) {
     return <div>No product id provided</div>
   }
-  const payload =  getPayload({
-    config,
+
+    // ✅ Await the client
+  const payload = await getPayload({ config })
+
+  // ✅ Await the findByID call
+  const product = await payload.findByID({
+    collection: 'product',
+    id,
   })
 
-  let product
+  // const payload =  getPayload({
+  //   config,
+  // })
 
-  try {
-    product =  (await payload).findByID({
-      collection: 'product',
-      id,
-    })
-  } catch (e) {
-    return <div>Product not found</div>
-  }
+  // let product
+
+  // try {
+  //   product =  (await payload).findByID({
+  //     collection: 'product',
+  //     id,
+  //   })
+  // } catch (e) {
+  //   return <div>Product not found</div>
+  // }
 //  const product  = await payload.findByID({ collection: 'product', id: `${id}`})
 
 
   return (
     <div className="grid place-items-center">
-      <h1></h1>
-{/* 
-          // <h1>{product.filename}</h1>
+   
+
+          <h1>{product.filename}</h1>
       <div className=' inset-0 flex items-center justify-center'>
 
           <img
@@ -44,7 +54,7 @@ export default async function Product({ searchParams }: PageProps) {
             alt={product.alt}
             className=" h-full object-cover"
           />
-      </div> */}
+      </div>
   
     </div>
   )
