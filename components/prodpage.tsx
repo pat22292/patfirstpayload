0ebfbfb55180dd172aff95777f181390bb27cae9
pat22292 <madlangawapat22@gmail.com>
2026-04-18 21:23:35 +0800

    test font altered

--- components/prodpage.tsx
+++ components/prodpage.tsx
@@ -94,9 +94,13 @@ export default function ProductPage({ product }: { product: any }) {
                   <path d="M19 6h-2c0-2.76-2.24-5-5-5S7 3.24 7 6H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-7-3c1.66 0 3 1.34 3 3H9c0-1.66 1.34-3 3-3zm7 17H5V8h14v12zm-7-8c-1.66 0-3-1.34-3-3H7c0 2.76 2.24 5 5 5s5-2.24 5-5h-2c0 1.66-1.34 3-3 3z" />
                 </svg>
               </div>
-              <span className="hidden font-bagwis md:block text-2xl font-bold tracking-tight">
-                ᜆᜒᜈ᜔ᜇᜑᜈ᜔
+              <span className="hidden font-[baybayin] md:block text-3xl  tracking-tight">
+                tindahan
+                {/* ᜆᜒᜈ᜔ᜇᜑᜈ᜔ */}
               </span>
+              {/* <span className="hidden font-bagwis md:block text-2xl font-bold tracking-tight">
+                ᜆᜒᜈ᜔ᜇᜑᜈ᜔
+              </span> */}
             </div>
           </Link>
           <div className="grow max-w-3xl flex flex-col pt-1">
